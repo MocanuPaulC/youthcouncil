6fc0e352e57aa54af10d08dbc095a6a576add762
Paul Mocanu <paul-cristian.mocanu@student.kdg.be>
2023-03-13 12:40:01 +0000

    Paul/usr str58

---
+++ src/main/java/be/kdg/youthcouncil/controllers/mvc/viewModels/CouncilAdminViewModel.java
@@ -0,0 +1,25 @@
+package be.kdg.youthcouncil.controllers.mvc.viewModels;
+
+import be.kdg.youthcouncil.domain.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CouncilAdminViewModel {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 8, max = 100, message = "Username should have length between 8 and 100")
+    private String email;
+    private String password = "password";
+    private Role role = Role.COUNCIL_ADMIN;
+
+
+}
