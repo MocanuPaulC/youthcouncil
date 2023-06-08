f47109dbb0ded154d8aae67b59407b25307eef76
Paul Mocanu <paul-cristian.mocanu@student.kdg.be>
2023-06-08 13:13:33 +0200

    fixed email to use secure smtp

--- src/main/java/be/kdg/youthcouncil/config/email/EmailConfig.java
+++ src/main/java/be/kdg/youthcouncil/config/email/EmailConfig.java
@@ -18,16 +18,17 @@ public class EmailConfig {
 	public JavaMailSender getJavaMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
+		mailSender.setPort(465);
 
 		mailSender.setUsername(emailProperties.getUsername());
 		mailSender.setPassword(emailProperties.getPassword());
 
 		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.transport.protocol", "smtps");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.socketFactory.fallback", "true");
 		props.put("mail.debug", "true");
+		props.put("mail.smtp.ssl.enable", "true");
 
 		return mailSender;
 	}
